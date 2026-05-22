9fa16037938cfd0af39a6302f81b3c886597d4a1
developerPapai <prechorus.melodies@gmail.com>
2026-05-22 22:08:17 +0530

    routes updated

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,17 +4,17 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    title: 'Papai | Full Stack Developer'
+    title: 'Home | developerPapai'
   },
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
-    title: 'Projects | Papai'
+    title: 'Projects | developerPapai'
   },
   {
     path: '**',
     loadComponent: () =>
       import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
-    title: 'Page Not Found | Papai',
+    title: 'Page Not Found | developerPapai',
   },
 ];
