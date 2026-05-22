6dcd9a58c17e1bc801adff2821fe0a2688d487cf
developerPapai <prechorus.melodies@gmail.com>
2026-05-22 22:53:26 +0530

    not found route added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,17 +4,17 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    title: 'Home | Portfolio'
+    title: 'Home'
   },
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
-    title: 'Projects | Portfolio'
+    title: 'Projects'
   },
   {
     path: '**',
     loadComponent: () =>
       import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
-    title: 'Page Not Found | Portfolio',
+    title: 'Page Not Found',
   },
 ];
