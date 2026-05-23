76e9314344837c66bd5bae462aa2e53bd5b4e16e
developerPapai <prechorus.melodies@gmail.com>
2026-05-23 18:03:07 +0530

    page title updated

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,17 +4,17 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    title: 'Home'
+    title: 'Home | developerPapai'
   },
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
-    title: 'Projects'
+    title: 'Projects | developerPapai'
   },
   {
     path: '**',
     loadComponent: () =>
       import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
-    title: 'Page Not Found',
+    title: 'Page Not Found | developerPapai',
   },
 ];
